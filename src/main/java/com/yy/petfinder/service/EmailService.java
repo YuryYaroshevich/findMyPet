cad3f816dc8648cfc8302d74275f2d86cc86e334
YuryYaroshevich <yury.yaroshevich@gmail.com>
2021-05-10 14:15:49 +0300

    wip email sending

--- src/main/java/com/yy/petfinder/service/EmailService.java
+++ src/main/java/com/yy/petfinder/service/EmailService.java
@@ -1,9 +1,12 @@
 package com.yy.petfinder.service;
 
 import com.yy.petfinder.rest.model.EmailMessageData;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.internet.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -23,13 +26,28 @@ public class EmailService {
   public Mono<Void> sendEmail(final String email, final EmailMessageData emailMessageData) {
     return Mono.fromRunnable(
         () -> {
-          final SimpleMailMessage message = new SimpleMailMessage();
+          /*final SimpleMailMessage message = new SimpleMailMessage();
           message.setFrom(appEmail);
           message.setTo(email);
           message.setSubject(emailMessageData.getSubject());
-
           message.setText(emailMessageData.getText());
-          emailSender.send(message);
+          emailSender.send(message);*/
+          final MimeMessage mimeMessage = emailSender.createMimeMessage();
+          try {
+            mimeMessage.setFrom(appEmail);
+            mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            mimeMessage.setSubject(emailMessageData.getSubject());
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent(emailMessageData.getText(), "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            mimeMessage.setContent(multipart);
+          } catch (MessagingException e) {
+            e.printStackTrace();
+          }
+          emailSender.send(mimeMessage);
         });
   }
 }
