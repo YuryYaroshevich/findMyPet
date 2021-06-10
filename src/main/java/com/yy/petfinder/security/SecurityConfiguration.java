1c4afbb4f2eae633c3346347c50082341ca0270f
YuryYaroshevich <yury.yaroshevich@gmail.com>
2021-06-10 16:43:07 +0300

    almost there; need to add vk oauth provider

--- src/main/java/com/yy/petfinder/security/SecurityConfiguration.java
+++ src/main/java/com/yy/petfinder/security/SecurityConfiguration.java
@@ -2,8 +2,13 @@ package com.yy.petfinder.security;
 
 import static com.yy.petfinder.util.PaginatedResponseHelper.NEXT_PAGE_TOKEN;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yy.petfinder.security.model.OAuthTokenWrapper;
 import com.yy.petfinder.security.service.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +19,16 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @EnableReactiveMethodSecurity
@@ -29,16 +38,19 @@ public class SecurityConfiguration {
   private final TokenService tokenService;
   private final OAuth2AuthenticationSuccessHandler authenticationSuccessHandler;
   private final OAuth2AuthenticationFailureHandler authenticationFailureHandler;
+  private final ObjectMapper objectMapper;
 
   public SecurityConfiguration(
       UserDetailsService reactiveUserDetailsService,
       TokenService tokenProvider,
       OAuth2AuthenticationSuccessHandler authenticationSuccessHandler,
-      OAuth2AuthenticationFailureHandler authenticationFailureHandler) {
+      OAuth2AuthenticationFailureHandler authenticationFailureHandler,
+      ObjectMapper objectMapper) {
     this.reactiveUserDetailsService = reactiveUserDetailsService;
     this.tokenService = tokenProvider;
     this.authenticationSuccessHandler = authenticationSuccessHandler;
     this.authenticationFailureHandler = authenticationFailureHandler;
+    this.objectMapper = objectMapper;
   }
 
   @Bean
@@ -112,5 +124,46 @@ public class SecurityConfiguration {
   @Bean
   public Pbkdf2PasswordEncoder passwordEncoder() {
     return new Pbkdf2PasswordEncoder();
+  }
+
+  /**
+   * VK oauth2 provider doesn't return token type and it breaks token parse workflow in spring
+   * security codebase. So we need to set custom webClient which will add token_type using filter.
+   */
+  @Bean
+  public WebClientReactiveAuthorizationCodeTokenResponseClient
+      webClientReactiveAuthorizationCodeTokenResponseClient() {
+    final WebClientReactiveAuthorizationCodeTokenResponseClient client =
+        new WebClientReactiveAuthorizationCodeTokenResponseClient();
+    client.setWebClient(
+        WebClient.builder()
+            .filter(
+                ExchangeFilterFunction.ofResponseProcessor(
+                    resp ->
+                        resp.bodyToMono(OAuthTokenWrapper.class)
+                            .map(token -> token.getFields())
+                            .map(
+                                fields -> {
+                                  if (fields.containsKey("token_type")) {
+                                    return fields;
+                                  } else {
+                                    final Map<String, Object> fieldsWithTokenType =
+                                        new HashMap<>(fields);
+                                    fieldsWithTokenType.put("token_type", "bearer");
+                                    return fieldsWithTokenType;
+                                  }
+                                })
+                            .map(fields -> new OAuthTokenWrapper(fields))
+                            .map(
+                                token -> {
+                                  try {
+                                    return objectMapper.writeValueAsString(token);
+                                  } catch (JsonProcessingException e) {
+                                    throw new RuntimeException(e);
+                                  }
+                                })
+                            .map(token -> ClientResponse.from(resp).body(token).build())))
+            .build());
+    return client;
   }
 }
