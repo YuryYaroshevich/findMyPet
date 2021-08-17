c3ae6113d55844484e5d41f0b174fef572206a3a
YuryYaroshevich <yury.yaroshevich@gmail.com>
2021-08-17 16:45:31 +0300

    fix

--- migrations/2021-08-16/createdAtFieldPetAd.js
+++ migrations/2021-08-16/createdAtFieldPetAd.js
@@ -4,7 +4,6 @@ var cursor = db.petAd.find();
 
 while(cursor.hasNext()) {
   var ad = cursor.next();
-  print(JSON.stringify(ad))
   var createdAt = ad._id.getTimestamp()
   db.petAd.update({_id: ad._id}, {$set: {createdAt: createdAt}})
 }
